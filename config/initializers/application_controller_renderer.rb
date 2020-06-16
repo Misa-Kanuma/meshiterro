2b801bebf3c4472922c267485a335735537f0a23
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-16 15:58:14 +0900

    [Update] 5-3 修正

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b60bcaa3529ca488452525ef0121fdc50d42330df50353a9122cac909ad6e655efcc7dc23575350a92afaccc8b5dfd89f3459dc8864c92dacb39c170975bdd05'
